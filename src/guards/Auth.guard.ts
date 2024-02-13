4cb3beb0adac33769eceaf0df2085ee614f0d4cc
Kawtious <mewmewby2@gmail.com>
2024-02-13 14:38:03 -0700

    Update license and remove license headers from code

--- src/guards/Auth.guard.ts
+++ src/guards/Auth.guard.ts
@@ -1,26 +1,3 @@
-/*
- * MIT License
- *
- * Copyright (c) 2023 Kawtious, Zeferito
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
@@ -28,7 +5,6 @@ import { Request } from 'express';
 
 import { ROLES_KEY } from '../decorators/Roles.decorator';
 import { Role } from '../enums/Role.enum';
-import { TokenVerificationError } from '../errors/TokenVerificationError';
 import { UserService } from '../services/User.service';
 
 @Injectable()
